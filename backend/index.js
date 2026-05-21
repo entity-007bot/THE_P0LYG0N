bb10ff322b10d27ecfc599a642b3c4edf7c85633
Obire Oviemena Israel <bossashura999k@gmail.com>
2026-05-21 18:42:22 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ seedJobs();
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Backend running on port ${PORT}`);
 });
 
